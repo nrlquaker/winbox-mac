2edd898872deae22679583fd462038a16b863e65
nrlquaker <ustynov.vy@gmail.com>
2019-10-21 23:18:32 +0300

    Update to 3.20

--- Winbox-mac.app/Contents/Resources/Wine.bundle/Contents/Resources/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/mike/Documents/wine/usr/lib"
-XML2_LIBS="-lxml2 -lz  -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/mike/Documents/wine/usr/include/libxml2"
-MODULE_VERSION="xml2-2.9.4"
-
--- Winbox-mac.app/Contents/Resources/Wine.bundle/Contents/Resources/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/mike/Documents/wine/usr/lib"
-XSLT_LIBS="-lxslt -L/Users/mike/Documents/wine/src/libxml2-2.9.4 -L/Users/mike/Documents/wine/usr/lib -lxml2 -lz -lpthread -liconv -lm  "
-XSLT_INCLUDEDIR="-I/Users/mike/Documents/wine/usr/include"
-MODULE_VERSION="xslt-1.1.29"
